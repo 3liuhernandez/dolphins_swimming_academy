e637ebb38fee5b5d09b4ac8aa2f3d21a3d63f067
Andres H <andresjhmndz@gmail.com>
2023-01-05 13:42:33 -0430

    deleting template

--- project/resources/views/layouts/includes/navbar.blade.php
+++ project/resources/views/layouts/includes/navbar.blade.php
@@ -1,59 +1,39 @@
-{{-- <!-- header section start -->
-<div class="header_section">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="logo"><a href="{{route('home')}}"></a></div>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+<nav class="navbar navbar-expand-lg navbar-light shadow p-0">
+    <div class="container">
+        <a class="navbar-brand m-0" href="{{ route('home') }}"><img src="{{ asset('imgs/logo_blue_bg_remove.png') }}" class="w-25" alt="logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon p-1"></span>
         </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                
-            </ul>
-        </div>
-    </nav>
-</div>
-<!-- header section end -->
---}}
-
-    <nav class="navbar navbar-expand-lg bg-dark navbar-light shadow p-0">
-        <div class="container">
-            <a class="navbar-brand m-0" href="#"><img src="{{ asset('imgs/logo_blue_bg_remove.png') }}" class="w-25" alt="logo"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                <span class="navbar-toggler-icon p-1"></span>
-            </button>
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h2 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h2>
-                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item active">
-                            <a class="nav-link p-2 text-white" href="{{ route('home') }}">HOME</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link p-2 text-white" href="trainings.html">TRAININGS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link p-2 text-white" href="classes.html">CLASSES</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link p-2 text-white" href="shows.html">SHOWS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link p-2 text-white" href="shortcodes.html">SHORTCODES</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link p-2 text-white" href="contact.html">CONTACT</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link p-2 text-white" href="{{ route('login') }}">INGRESO</a>
-                        </li>
-                    </ul>
-                </div>
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h2 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h2>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('home') }}">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">TRAININGS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">CLASSES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">SHOWS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">SHORTCODES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">CONTACT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">INGRESO</a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </nav>
+    </div>
+</nav>
--- project/resources/views/layouts/master.blade.php
+++ project/resources/views/layouts/master.blade.php
@@ -14,41 +14,6 @@
     <!-- BOOTSTRAP 5.3 -->
     <link rel="stylesheet" type="text/css" href="{{ asset('vendor/bs/css/bootstrap.min.css') }}">
 
-    <!-- BOOTSTRAP JAVASCRIPT -->
-    <link rel="stylesheet" href="{{ asset('bs/js/bootstrap.bundle.min.js') }}">
-    <link rel="stylesheet" href="{{ asset('bs/js/bootstrap.min.js') }}">
-
-    <!-- VENDOR ICONS -->
-    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('vendor/mdi/css/materialdesignicons.min.css') }}" media="all" /> --}}
-
-    {{-- <link rel="stylesheet" href="{{ asset('vendor/snackbar/custom_snackbar.css') }}"> --}}
-    {{-- <link rel="stylesheet" href="{{ asset('vendor/toast/toast.css') }}"> --}}
-
-    <!-- BOOTSTRAP ICONS -->
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"> --}}
-
-    <!-- SLICK SLIDER -->
-    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('vendor/slick/slick.css') }}"> --}}
-    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('vendor/slick/slick-theme.css') }}"> --}}
-
-
-    {{-- STYLES TEMPLATE --}}
-
-    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('template/css/style.css') }}"> --}}
-
-    <!-- Responsive-->
-    {{-- <link rel="stylesheet" href="{{ asset('template/css/responsive.css') }}"> --}}
-    <!-- Scrollbar Custom CSS -->
-    {{-- <link rel="stylesheet" href="{{ asset('template/css/jquery.mCustomScrollbar.min.css') }}"> --}}
-    <!-- Tweaks for older IEs-->
-    {{-- <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"> --}}
-    <!-- owl stylesheets -->
-    {{-- <link rel="stylesheet" href="{{ asset('template/css/owl.carousel.min.css') }}"> --}}
-
-    {{-- <link rel="stylesheet" href="{{ asset('template/css/owl.theme.default.min.css') }}"> --}}
-
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen"> --}}
-
     @yield('header')
 
 </head>
@@ -81,52 +46,30 @@
 
     @include('layouts.includes.footer')
 
-    <!-- copyright section start -->
-    <div class="copyright_section">
-        <div class="container">
-            <p class="copyright_text">2020 All Rights Reserved. Design by <a href="https://html.design">Free html Templates</a></p>
-        </div>
-    </div>
-
     <!-- CDN SLICE -->
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/bs/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('vendor/popper/popper.min.js') }}"></script>
+    {{-- <script src="{{ asset('vendor/popper/popper.min.js') }}"></script> --}}
 
-    <script src="{{ asset('vendor/slick/slick.min.js') }}"></script>
+    {{-- <script src="{{ asset('vendor/slick/slick.min.js') }}"></script> --}}
 
-    <script src="{{ asset('vendor/snackbar/custom_snackbar.js') }}"></script>
-    <script src="{{ asset('vendor/toast/toast.js') }}"></script>
+    {{-- <script src="{{ asset('vendor/snackbar/custom_snackbar.js') }}"></script> --}}
+    {{-- <script src="{{ asset('vendor/toast/toast.js') }}"></script> --}}
 
     <!-- MY APP JS -->
-    <script src="{{ asset('js/slice-carusel.js') }}" id="js_slick"></script>
+    {{-- <script src="{{ asset('js/slice-carusel.js') }}" id="js_slick"></script> --}}
     <!-- TOOLTIP TIGGER -->
-    <script src="{{ asset('js/tooltip_trigger.js') }}"></script>
+    {{-- <script src="{{ asset('js/tooltip_trigger.js') }}"></script> --}}
 
-    <script src="{{ asset('js/master.js') }}"></script>
+    {{-- <script src="{{ asset('js/master.js') }}"></script> --}}
 
-    <script src="{{ asset('vendor/axios/axios.min.js') }}"></script>
+    {{-- <script src="{{ asset('vendor/axios/axios.min.js') }}"></script> --}}
 
-
-    {{-- JS TEMPLATE --}}
-    <!-- Javascript files-->
-    <script src="{{asset('template/js/jquery.min.js')}}"></script>
-    <script src="{{asset('template/js/popper.min.js')}}"></script>
-    <script src="{{asset('template/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('template/js/jquery-3.0.0.min.js')}}"></script>
-    <script src="{{asset('template/js/plugin.js')}}"></script>
-    <!-- sidebar -->
-    <script src="{{asset('template/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
-    <script src="{{asset('template/js/custom.js')}}"></script>
-    <!-- javascript -->
-    <script src="{{asset('template/js/owl.carousel.js')}}"></script>
-    <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
-
-    <script>
+    {{-- <script>
         jQuery(() => {
-            // show_toast('mensaje que va en el toast');
+            show_toast('mensaje que va en el toast');
         });
-    </script>
+    </script> --}}
 
     @yield('footer')
 </body>
